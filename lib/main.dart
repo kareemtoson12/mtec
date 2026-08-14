12d47b8f1dfff9493f3e076d12b7838913797d45
kareemtoson-creator <kareemtoson-creator@github.com>
2026-08-14 14:23:50 +0300

    Refactor: Simplify main app structure and remove unused screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,5 @@
-import 'package:flutter/material.dart';
-import 'package:mtec/task2.dart';
+// Karim Toson || kareemtoson1@gmail.com || Fri Aug 14 2026 14:19:25
 
-void main() {
-  runApp(MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: Task2());
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        height: double.infinity,
-        color: Colors.red,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-
-          children: [
-            Text(
-              'hello world',
-              style: TextStyle(
-                color: Colors.amber,
-                fontSize: 30,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            Text(
-              'hello world',
-              style: TextStyle(
-                color: Colors.amber,
-                fontSize: 30,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+/* abc */
