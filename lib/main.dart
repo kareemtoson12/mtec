7ae4c727e09361df671a358f856d3581957db207
kareemtoson12 <chatg0160@gmail.com>
2026-08-21 14:35:12 +0300

    feat: initialize base Flutter application structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,14 @@
-void main()
-{
-  
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
 }
